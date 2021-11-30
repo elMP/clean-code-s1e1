314684778188838d2c56d51fc7c21ed5e5110fe3
kneazle <kneazle@yandex.ru>
2021-11-30 11:19:08 +0300

    refactor: add classes to some selectors according rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -34,18 +34,20 @@ var createNewTaskElement = function (taskString) {
   var deleteButtonImg = document.createElement("img");//delete button image
 
   label.innerText = taskString;
-  label.className = 'task';
+  label.className = 'task task-label';
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
+  checkBox.className = "task-checkbox";
   editInput.type = "text";
-  editInput.className = "task";
+  editInput.className = "task task-input";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
   editButton.className = "button edit";
 
   deleteButton.className = "button delete";
   deleteButtonImg.src = './remove.svg';
+  deleteButtonImg.className = "remove-picture";
   deleteButton.appendChild(deleteButtonImg);
 
 
